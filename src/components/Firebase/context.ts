02ac206fe00a7472950f0732f0fae038d4e0f5a9
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-06 08:36:39 +0200

    wrap app with firebase provider

--- src/components/Firebase/context.ts
+++ src/components/Firebase/context.ts
@@ -1,5 +1,6 @@
 import React from 'react'
+import Firebase from "./Firebase";
 
-const FirebaseContext = React.createContext(null);
+const FirebaseContext = React.createContext(null as null | Firebase);
 
 export default FirebaseContext
---
+++ src/components/Firebase/index.ts
@@ -0,0 +1,8 @@
+import Firebase from './Firebase'
+import FirebaseContext from './context'
+
+export default Firebase
+
+export {
+    FirebaseContext,
+}
