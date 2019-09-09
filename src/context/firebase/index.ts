8d2110ccfd61094b4a63f1ae0606d598cf30f838
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-09 10:20:31 +0200

    firebase context added

---
+++ src/context/firebase/index.ts
@@ -0,0 +1,9 @@
+import Firebase from './Firebase'
+import FirebaseContext, {useFirebaseContext} from './context'
+
+export default FirebaseContext
+
+export {
+    Firebase,
+    useFirebaseContext,
+}
