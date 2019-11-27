5e4c4928dfbe21bdae84cd3a592f57feb05eefb9
Bernard Bussy <brbitzbussy@gmail.com>
2019-11-27 21:47:51 +0200

    add expiration method to login claims

--- src/bizzle/security/claims/Login.ts
+++ src/bizzle/security/claims/Login.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import {parse} from './parse';
 import {ClaimsType} from './types';
 
@@ -20,5 +21,23 @@ export default class Login {
         }
         this.userID = login.userID;
         this.expirationTime = login.expirationTime;
+    }
+
+    /**
+     * returns milliseconds to expiry
+     * @returns {number}
+     */
+    get timeToExpiry(): number {
+        return moment.unix(this.expirationTime)
+            .utc()
+            .diff(moment.utc(), 'seconds') * 1000;
+    }
+
+    /**
+     * Whether these login are expired or not
+     * @returns {boolean}
+     */
+    get notExpired(): boolean {
+        return moment.utc().isBefore(moment.unix(this.expirationTime).utc());
     }
 }
