40aaae54aaf7bf0485c32aac61c5158ae034ba45
Bernard Bussy <brbitzbussy@gmail.com>
2019-12-03 16:37:57 +0200

    add sessions to tab

--- src/routes/private/routes.ts
+++ src/routes/private/routes.ts
@@ -7,6 +7,7 @@ import {RouteType} from 'routes/Route';
 import ExerciseView from 'views/Exercise';
 import {Party1HomeView} from 'views/Home';
 import ProfileView from 'views/Profile';
+import SessionView from 'views/Session';
 
 export const routes: RouteType[] = [
     // single route, no collapsible menu
@@ -15,6 +16,16 @@ export const routes: RouteType[] = [
         path: '/app/exercise',
         icon: MoneyIcon,
         component: ExerciseView,
+
+        // views null since collapse is false
+        collapse: false,
+        views: null
+    },
+    {
+        name: 'Session',
+        path: '/app/session',
+        icon: MoneyIcon,
+        component: SessionView,
 
         // views null since collapse is false
         collapse: false,
