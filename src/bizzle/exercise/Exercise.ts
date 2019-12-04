40abfec414c8d0bedc0ff3e6ec78ae775d96e653
Bernard Bussy <brbitzbussy@gmail.com>
2019-12-04 08:54:32 +0200

    add extra muscle groups

--- src/bizzle/exercise/Exercise.ts
+++ src/bizzle/exercise/Exercise.ts
@@ -1,6 +1,9 @@
 export enum MuscleGroup {
     BicepsMuscleGroup = 'Biceps',
-    PectoralMuscleGroup = 'Pectoral'
+    PectoralMuscleGroup = 'Pectoral',
+    ShouldersMuscleGroup = 'Shoulders',
+    CoreMuscleGroup = 'Core',
+    LegsMuscleGroup = 'Legs'
 }
 
 export default class Exercise {
