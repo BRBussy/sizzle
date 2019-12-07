90915255360629cfcd94f318503b935822427a20
Bernard Bussy <brbitzbussy@gmail.com>
2019-12-07 14:46:11 +0200

    ignore exhaustive dependencies

--- src/context/App/App.tsx
+++ src/context/App/App.tsx
@@ -52,6 +52,7 @@ const AppContext: React.FC = ({children}: { children?: React.ReactNode }) => {
         } catch (e) {
             console.error('error determining logged in from local storage');
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const logout = () => {
