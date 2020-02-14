81d1d499bb55d90284ff647814a62706fbbac205
Bernard Bussy <brbitzbussy@gmail.com>
2020-02-14 16:57:36 +0200

    basic

--- src/views/Budget/ViewBudgets/ViewBudgets.tsx
+++ src/views/Budget/ViewBudgets/ViewBudgets.tsx
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import {
-    makeStyles, Theme, createStyles, Grid, TextField
+    makeStyles, Theme, createStyles, Grid, TextField,
+    Card, CardContent, AppBar, Tabs, Tab, CardHeader
 } from '@material-ui/core';
-import {BudgetAdmin} from 'bizzle/budget';
+import {Budget, BudgetAdmin} from 'bizzle/budget';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-    root: {}
+    root: {
+        display: 'grid',
+        gridTemplateRows: 'auto 1fr',
+        gridRowGap: theme.spacing(1)
+    }
 }));
 
 let getBudgetForDateRangeTimeout: any;
@@ -14,6 +19,8 @@ const ViewBudgets = () => {
     const classes = useStyles();
     const [startDate, setStartDate] = useState<string | undefined>(undefined);
     const [endDate, setEndDate] = useState<string | undefined>(undefined);
+    const [budget, setBudget] = useState(new Budget());
+    const [selectedBudgetTab, setSelectedBudgetTab] = useState('Summary');
 
     useEffect(() => {
         const getBudgetForDateRange = async () => {
@@ -21,10 +28,10 @@ const ViewBudgets = () => {
                 return;
             }
             try {
-                await BudgetAdmin.GetBudgetForDateRange({
+                setBudget((await BudgetAdmin.GetBudgetForDateRange({
                     startDate: `${startDate}T00:00:00Z`,
                     endDate: `${endDate}T00:00:00Z`
-                });
+                })).budget);
             } catch (e) {
                 console.error(`error getting budget for date range: ${e.message ? e.message : e.toString()}`)
             }
@@ -33,38 +40,68 @@ const ViewBudgets = () => {
         getBudgetForDateRangeTimeout = setTimeout(getBudgetForDateRange, 400);
     }, [startDate, endDate]);
 
+    const handleSelectedBudgetTabChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setSelectedBudgetTab(newValue);
+    };
+
     return (
         <div className={classes.root}>
-            <Grid container direction={'row'} spacing={1}>
-                <Grid item>
-                    <TextField
-                        label={'Start Date'}
-                        type={'date'}
-                        InputLabelProps={{shrink: true}}
-                        onChange={(e) => {
-                            if (!e.target.value || e.target.value === '') {
-                                setStartDate(undefined);
-                            } else {
-                                setStartDate(e.target.value);
-                            }
-                        }}
-                    />
-                </Grid>
-                <Grid item>
-                    <TextField
-                        label={'End Date'}
-                        type={'date'}
-                        InputLabelProps={{shrink: true}}
-                        onChange={(e) => {
-                            if (!e.target.value || e.target.value === '') {
-                                setEndDate(undefined);
-                            } else {
-                                setEndDate(e.target.value);
-                            }
-                        }}
-                    />
-                </Grid>
-            </Grid>
+            <Card>
+                <CardContent>
+                    <Grid container direction={'row'} spacing={1}>
+                        <Grid item>
+                            <TextField
+                                label={'Start Date'}
+                                type={'date'}
+                                InputLabelProps={{shrink: true}}
+                                onChange={(e) => {
+                                    if (!e.target.value || e.target.value === '') {
+                                        setStartDate(undefined);
+                                    } else {
+                                        setStartDate(e.target.value);
+                                    }
+                                }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                label={'End Date'}
+                                type={'date'}
+                                InputLabelProps={{shrink: true}}
+                                onChange={(e) => {
+                                    if (!e.target.value || e.target.value === '') {
+                                        setEndDate(undefined);
+                                    } else {
+                                        setEndDate(e.target.value);
+                                    }
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader
+                    title={
+                        <AppBar position='static'>
+                            <Tabs
+                                value={selectedBudgetTab}
+                                onChange={handleSelectedBudgetTabChange}
+                                variant={'scrollable'}
+                                scrollButtons={'auto'}
+                            >
+                                <Tab label={'Summary'} value={'Summary'}/>
+                                {Object.keys(budget.entries).map((budgetEntryCategory, idx) => (
+                                    <Tab key={idx} label={budgetEntryCategory} value={budgetEntryCategory}/>
+                                ))}
+                            </Tabs>
+                        </AppBar>
+                    }
+                />
+                <CardContent>
+                    content
+                </CardContent>
+            </Card>
         </div>
     )
 };
