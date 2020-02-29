7bbda6e03382e7f444bd32006b351d92c39cba04
Bernard <brbitzbussy@gmail.com>
2020-02-29 16:09:53 +0200

    add update one and update many methods to service provider

--- src/bizzle/budget/entry/Admin.ts
+++ src/bizzle/budget/entry/Admin.ts
@@ -33,6 +33,24 @@ export interface CreateManyRequest {
 export interface CreateManyResponse {
 }
 
+export interface UpdateOneRequest {
+    budgetEntry: BudgetEntry;
+}
+
+// tslint:disable-next-line:no-empty-interface
+export interface UpdateOneResponse {
+
+}
+
+export interface UpdateManyRequest {
+    budgetEntries: BudgetEntry[];
+}
+
+// tslint:disable-next-line:no-empty-interface
+export interface UpdateManyResponse {
+
+}
+
 const Admin = {
     serviceProvider: 'BudgetEntry-Admin',
     async XLSXStandardBankStatementToBudgetEntries(request: XLSXStandardBankStatementToBudgetCompositeEntriesRequest): Promise<XLSXStandardBankStatementToBudgetCompositeEntriesResponse> {
@@ -67,6 +85,20 @@ const Admin = {
             method: `${Admin.serviceProvider}.CreateMany`,
             request
         });
+    },
+    async UpdateOne(request: UpdateOneRequest): Promise<UpdateOneResponse> {
+        return await jsonRPCRequest({
+            url: config.get('budgetURL'),
+            method: `${Admin.serviceProvider}.UpdateOne`,
+            request
+        })
+    },
+    async UpdateMany(request: UpdateManyRequest): Promise<UpdateManyResponse> {
+        return await jsonRPCRequest({
+            url: config.get('budgetURL'),
+            method: `${Admin.serviceProvider}.UpdateMany`,
+            request
+        })
     }
 };
 
